76e132014a774d47b36726eda21d6125c9f7a032
PranavAgarwal3112 <143040759+PranavAgarwal3112@users.noreply.github.com>
2024-10-20 16:41:30 +0530

    CalcMaster

---
+++ calc.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("display")
+let buttons = document.querySelectorAll(".box")
+let string = ""
+
+Array.from(buttons).forEach((box)=>{
+    box.addEventListener('click' ,(e)=>{
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            input.value=string
+        }
+       else if(e.target.innerHTML == 'C'){
+            string="0";
+            input.value=string;
+        }
+       else if(e.target.innerHTML == 'DEL'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
+        console.log(e.target);
+        string=string+e.target.innerHTML;
+        input.value=string;
+        }
+    })
+})
